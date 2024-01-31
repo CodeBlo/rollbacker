1957ddbeb2ea81fa7624b8df00f02ef51e4051c4
CodeBlo <abdulkadirelmaci@gmail.com>
2024-02-01 02:37:10 +0300

    Add executor simple aspect for rollback operations

---
+++ src/main/java/org/rollbacker/annotation/Executor.java
@@ -0,0 +1,15 @@
+package org.rollbacker.annotation;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Executor {
+    String value() default "";
+
+    Class<? extends Throwable>[] ignoredExceptions() default {};
+}
---
+++ src/main/java/org/rollbacker/aspect/ExecutorAspect.java
@@ -0,0 +1,81 @@
+package org.rollbacker.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.rollbacker.annotation.Exceptioner;
+import org.rollbacker.annotation.Executor;
+import org.rollbacker.annotation.Rollbacker;
+import org.slf4j.Logger;
+
+import java.lang.reflect.Method;
+
+@Aspect
+public class ExecutorAspect {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(ExecutorAspect.class);
+
+    @DeclareError("@annotation(org.rollbacker.annotation.Exceptioner) && !execution(* *(java.lang.Throwable+))")
+    static final String ERROR_MESSAGE = "Exceptioner should only have 1 argument which takes a throwable";
+
+    @Around("@annotation(executor)")
+    public Object around(ProceedingJoinPoint joinPoint, Executor executor) {
+        try {
+            return joinPoint.proceed();
+        } catch (Throwable e) {
+
+            Class<? extends Throwable>[] ignoredExceptions = executor.ignoredExceptions();
+            for (Class<? extends Throwable> ignoredException : ignoredExceptions) {
+                if (ignoredException.isAssignableFrom(e.getClass())) {
+                    return null;
+                }
+            }
+            String tag = executor.value();
+            Object aThis = joinPoint.getThis();
+            exceptioner(aThis, tag, e);
+            log.error("Error while running method {}. Rolling back...", joinPoint.toShortString());
+            rollback(aThis, tag, joinPoint.toShortString());
+        }
+        return null;
+    }
+
+    private void exceptioner(Object aspectedObject, String tag, Throwable throwable) {
+        Method[] declaredMethods = aspectedObject.getClass().getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            Exceptioner exceptioner = declaredMethod.getAnnotation(Exceptioner.class);
+            if (exceptioner == null || !exceptioner.value().equals(tag)) {
+                continue;
+            }
+            Class<?> parameterType = declaredMethod.getParameterTypes()[0];
+            if (!parameterType.isAssignableFrom(throwable.getClass())) {
+                continue;
+            }
+            declaredMethod.setAccessible(true);
+            try {
+                declaredMethod.invoke(aspectedObject, throwable);
+            } catch (Exception ex) {
+                log.error("Error while invoking exceptioner method", ex);
+            }
+            break;
+        }
+    }
+
+    private void rollback(Object aspectedObject, String tag, String methodName) {
+        Method[] declaredMethods = aspectedObject.getClass().getDeclaredMethods();
+        for (Method declaredMethod : declaredMethods) {
+            Rollbacker rollbacker = declaredMethod.getAnnotation(Rollbacker.class);
+            if (rollbacker == null || !rollbacker.value().equals(tag)) {
+                continue;
+            }
+
+            declaredMethod.setAccessible(true);
+            try {
+                declaredMethod.invoke(aspectedObject);
+            } catch (Exception ex) {
+                log.error("Error while executing rollback for method:{}", methodName);
+            }
+            break;
+        }
+    }
+
+
+}
