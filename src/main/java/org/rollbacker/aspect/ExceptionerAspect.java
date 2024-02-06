8715f0209ed2dcb0c826f4537b7ef9aa64f97e9d
CodeBlo <abdulkadirelmaci@gmail.com>
2024-02-06 22:12:12 +0300

    Convert aspect to exception

--- src/main/java/org/rollbacker/aspect/ExceptionerAspect.java
+++ src/main/java/org/rollbacker/aspect/ExceptionerAspect.java
@@ -9,6 +9,6 @@ import org.aspectj.lang.annotation.DeclareError;
  */
 @Aspect
 public class ExceptionerAspect {
-    @DeclareError("@annotation(org.rollbacker.annotation.Exceptioner) && !execution(* *(java.lang.Throwable+))")
+    @DeclareError("@annotation(org.rollbacker.annotation.Exceptioner) && !execution(* *(java.lang.Exception+))")
     static final String ERROR_MESSAGE = "Exceptioner should only have 1 argument which argument class should be a subclass of throwable";
 }
